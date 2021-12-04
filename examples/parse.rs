9dca3f563f8e6847b50610291a9d3a521c57a253
Ch0ronomato <schweerian33@gmail.com>
2021-12-04 12:04:57 -0800

    Simple example

--- examples/parse.rs
+++ examples/parse.rs
@@ -5,8 +5,16 @@ use std::fs::File;
 
 fn main() {
     if let Ok(file) = File::open("./data/wave.aif") {
-
+        let mut reader = AiffReader::new(file);
+        match reader.read() {
+            Ok(_) => {
+                println!("{:?}", reader.samples::<f32>())
+            },
+            Err(e) => {
+                panic!("Failed to read file {:?}", e)
+            }
+        }
     } else {
-
+        panic!("NOOO")
     }
 }
